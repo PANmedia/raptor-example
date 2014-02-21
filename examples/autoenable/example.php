e15129cb5a0c480041b87e61dd7bb083788a0283
David Neilsen <david@panmedia.co.nz>
2014-02-21 16:50:47 +1300

    Add examples/autoenable/example.php

---
+++ examples/autoenable/example.php
@@ -0,0 +1,26 @@
+<?php
+    include __DIR__ . '/../../include.php';
+    $example = new Raptor\Example('Autoenable');
+?>
+<!doctype html>
+<html>
+<head>
+    <?= $example->renderHead(); ?>
+    <script type="text/javascript">
+        init(function($) {
+            $('.editable').raptor(extendDefaults({
+                autoEnable: true
+            }));
+        });
+    </script>
+</head>
+<body>
+    <?= $example->renderNavigation(); ?>
+    <div class="source-watch" data-output="#source" data-target=".editable">
+        <div class="editable center half" data-id="body-1">
+            <?= $example->renderContent('body-1', $example->getDefaultContent()); ?>
+        </div>
+    </div>
+    <div id="source" class="center half"></div>
+</body>
+</html>
