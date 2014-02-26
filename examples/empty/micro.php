5cd2022dd5b67504e220ed6dda934187415cccfd
David Neilsen <david@panmedia.co.nz>
2014-02-27 10:06:10 +1300

    Add a micro example for empty elements

---
+++ examples/empty/micro.php
@@ -0,0 +1,33 @@
+<?php
+    include __DIR__ . '/../../include.php';
+    $example = new Raptor\Example('Empty');
+?>
+<!doctype html>
+<html>
+<head>
+    <?= $example->renderHead(); ?>
+    <script type="text/javascript">
+        init(function($) {
+            $('.editable').raptor(extendDefaults({
+                preset: 'micro'
+            }));
+        });
+    </script>
+    <style type="text/css">
+        .editable {
+            margin-top: 10px;
+            min-height: 10px;
+            border: 1px dotted black;
+        }
+    </style>
+</head>
+<body>
+    <?= $example->renderNavigation(); ?>
+    <div class="source-watch" data-output="#source" data-target=".editable">
+        <div class="editable center half" data-id="body-1">
+            <?= $example->renderContent('body-1', ''); ?>
+        </div>
+    </div>
+    <div id="source" class="center half"></div>
+</body>
+</html>
