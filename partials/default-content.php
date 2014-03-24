c8be397e641111b5915cf12d9f541b72e204932f
David Neilsen <david@panmedia.co.nz>
2014-03-25 09:22:28 +1300

    Update partials/default-content.php

--- partials/default-content.php
+++ partials/default-content.php
@@ -1,19 +1,24 @@
-<h1>Raptor Editor - Basic Example</h1>
+<h1>Raptor Editor - Default Content</h1>
 <p>
     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
     has been the industry's standard dummy text ever since the 1500s, when an unknown printer
     took a galley of type and scrambled it to make a type specimen book.
 </p>
+<p>
+    It has survived not only five centuries, but also the leap into electronic typesetting,
+    remaining essentially unchanged.
+</p>
 <blockquote>
     <p>
-        It has survived not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+        including versions of Lorem Ipsum.
     </p>
 </blockquote>
 <p>
-    It was popularised in the 1960s with the release of Letraset sheets containing
-    Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-    including versions of Lorem Ipsum.
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+    has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+    took a galley of type and scrambled it to make a type specimen book.
 </p>
 <p>
     <span class="cms-blue">This text is blue.</span>
