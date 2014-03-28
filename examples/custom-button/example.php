1b291f16de3528e7e47abe132a8ce1499981db51
David Neilsen <david@panmedia.co.nz>
2014-03-28 13:32:54 +1300

    Update examples/custom-button/example.php

--- examples/custom-button/example.php
+++ examples/custom-button/example.php
@@ -1,22 +1,12 @@
 <?php
-    include __DIR__ . '/../include/content.php';
-    $content = loadContent(__DIR__ . '/content.json');
+    include __DIR__ . '/../../include.php';
+    $example = new Raptor\Example('Custom Button');
 ?>
 <!doctype html>
 <html>
 <head>
-    <?php include __DIR__ . '/../include/head.php'; ?>
-    <title>Raptor Editor - Basic Example</title>
+    <?= $example->renderHead(); ?>
     <script type="text/javascript">
-        // Test for no conflict version
-        var init;
-        if (typeof raptor !== 'undefined') {
-            init = raptor;
-        } else if (typeof jQuery !== 'undefined') {
-            init = jQuery;
-        } else {
-            alert('Could not find initialiser');
-        }
         Raptor.extendLocale('en', {
             customButtonTitle: 'My Custom Button'
         });
@@ -26,24 +16,19 @@
                 Raptor.selectionReplace('<b>My super awesome button</b>');
             }
         }));
-        Raptor.defaults.layouts.toolbar.uiOrder[0].unshift('customButton');
+        Raptor.presets[preset].layouts.toolbar.uiOrder[0].unshift('customButton');
         init(function($) {
-            $('#left').raptor({
-                urlPrefix: '../../src/'
-            });
+            $('.editable').raptor(defaultOptions);
         });
-
     </script>
 </head>
 <body>
-    <?php include __DIR__ . '/../include/nav.php'; ?>
-    <div class="editable half center" id="left" data-id="body-1">
-        <?php ob_start(); ?>
-            <h1>Raptor Editor - Custom Button Example</h1>
-            <p>
-                Custom button example.
-            </p>
-        <?= renderContent(ob_get_clean(), $content, 'body-1'); ?>
+    <?= $example->renderNavigation(); ?>
+    <div class="source-watch" data-output="#source" data-target=".editable">
+        <div class="editable center half" data-id="body-1">
+            <?= $example->renderContent('body-1', $example->getDefaultContent()); ?>
+        </div>
     </div>
+    <div id="source" class="center half"></div>
 </body>
 </html>
