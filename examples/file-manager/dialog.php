1301d550c4d3d470232fbc00c6173b3286edab3a
David Neilsen <david@panmedia.co.nz>
2014-06-19 08:21:11 +1200

    Update examples/file-manager/dialog.php

--- examples/file-manager/dialog.php
+++ examples/file-manager/dialog.php
@@ -2,13 +2,7 @@
 <!doctype html>
 <html>
 <head>
-    <?php
-        require_once RAPTOR_THEMES_DIR . 'include.php';
-        require_once RAPTOR_DEPENDENCIES_DIR . 'include.php';
-        require_once RAPTOR_COMMON_DIR . 'include.php';
-        require_once RAPTOR_LOCALES_DIR . 'include.php';
-        require_once RAPTOR_FILE_MANAGER_DIR . 'src/include.php';
-    ?>
+    <?php include __DIR__ . '/../../partials/head.php'; ?>
     <style type="text/css">
         body {
             height: 3000px;
@@ -21,11 +15,17 @@
     </style>
 </head>
 <body>
+    <button id="open">Open</button>
     <script type="text/javascript">
-        RFM.dialog({
+        var rfm = RFM.dialog({
             uriAction: '<?= RAPTOR_EXAMPLE_URI; ?>actions/file-manager.php',
             uriIcon: '<?= RAPTOR_EXAMPLE_URI; ?>examples/file-manager/icon/'
+        }, {
+            autoOpen: false
         });
+        jQuery('#open').click(function() {
+            jQuery(rfm.node).dialog('open');
+        })
     </script>
 </body>
 </html>
