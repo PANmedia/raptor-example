218165fc3ff72ed53393f6c38bd4f095e443a169
David Neilsen <david@panmedia.co.nz>
2014-07-03 10:20:33 +1200

    Update examples/inline/example.php

--- examples/inline/example.php
+++ examples/inline/example.php
@@ -15,21 +15,42 @@
             }));
         });
     </script>
+    <style>
+        body {
+            background-color: #555;
+            color: #eee;
+        }
+        #source {
+            background-color: white;
+            border: 1px solid #c1c1c1;
+        }
+    </style>
 </head>
 <body>
     <?= $example->renderNavigation(); ?>
     <div class="wrapper center">
-        <?php
-            if (isset($_GET['status'])) {
-                if ($_GET['status'] == 'success') {
-                    echo '<p>Successfully saved comment.</p>';
-                } elseif ($_GET['status'] == 'failed') {
-                    echo '<p>Failed to save comment.</p>';
-                }
-            }
-        ?>
         <form action="<?= RAPTOR_EXAMPLE_URI; ?>actions/save-comment.php" method="post" class="center half">
-            <input type="hidden" name="redirect" value="<?= $_SERVER['REQUEST_URI']; ?>" />
+            <?php if (isset($_GET['status']) && $_GET['status'] == 'success'): ?>
+                <div class="ui-widget ui-notification">
+                    <div class="ui-state-confirmation ui-corner-all">
+                        <p>
+                            <span class="ui-icon ui-icon-error"></span>
+                            <strong>Success:</strong> Successfully saved comment.
+                        </p>
+                    </div>
+                </div>
+            <?php elseif (isset($_GET['status']) && $_GET['status'] == 'failed'): ?>
+                <div class="ui-widget ui-notification">
+                    <div class="ui-state-error ui-corner-all">
+                        <p>
+                            <span class="ui-icon ui-icon-error"></span>
+                            <strong>Alert:</strong> Failed to save comment. <?= isset($_GET['message'])  ? $_GET['message'] : ''; ?>
+                        </p>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+            <input type="hidden" name="redirect" value="<?= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); ?>" />
             <h1>Raptor Inline Example</h1>
             <div class="source-watch" data-output="#source" data-target=".editable">
                 <textarea name="comment" class="editable"></textarea>
