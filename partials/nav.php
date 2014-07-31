736a30cbd74cefd274adc0a3ce953e760678ebed
David Neilsen <david@panmedia.co.nz>
2014-08-01 10:56:37 +1200

    Add title to packaged raptor.js location in partials/nav.php

--- partials/nav.php
+++ partials/nav.php
@@ -1,7 +1,7 @@
 <div class="switcher-spacer"></div>
 <nav class="switcher">
     <a href="?">Include all files individually</a>
-    <a href="?type=packaged">Use a packaged version of Raptor</a>
+    <a href="?type=packaged" title="/packages/raptor.js">Use a packaged version of Raptor</a>
     <a href="<?= RAPTOR_EXAMPLE_URI; ?>/actions/clear-data.php">Clear Saved Data</a>
 </nav>
 <div style="clear: both"></div>
