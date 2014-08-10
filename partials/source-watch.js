22552c0464d515a6ee7a0ae4ba994904c86380ba
David Neilsen <david@panmedia.co.nz>
2014-08-11 08:57:53 +1200

    Wrap partials/source-watch.js

--- partials/source-watch.js
+++ partials/source-watch.js
@@ -1,30 +1,32 @@
-setInterval(function() {
-    $('.source-watch').each(function() {
-        var target = $(this).data('target');
-        if (target) {
-            target = $(target);
-        } else {
-            target = $(this);
-        }
-        var html = target.html();
-        if (this.previousHtml != html) {
-            this.previousHtml = html
-            html = style_html(html, {
-                max_char: 0
-            });
-
-            var output = $(this).data('output');
-
-            if (typeof CodeMirror !== 'undefined') {
-                $(output).html('');
-                CodeMirror($(output).get(0), {
-                    value: html,
-                    mode: 'htmlmixed'
-                });
+(function($) {
+    setInterval(function() {
+        $('.source-watch').each(function() {
+            var target = $(this).data('target');
+            if (target) {
+                target = $(target);
             } else {
-                $(output).text(html);
+                target = $(this);
             }
-        }
+            var html = target.html();
+            if (this.previousHtml != html) {
+                this.previousHtml = html
+                html = style_html(html, {
+                    max_char: 0
+                });
 
-    });
-}, 400);
+                var output = $(this).data('output');
+
+                if (typeof CodeMirror !== 'undefined') {
+                    $(output).html('');
+                    CodeMirror($(output).get(0), {
+                        value: html,
+                        mode: 'htmlmixed'
+                    });
+                } else {
+                    $(output).text(html);
+                }
+            }
+
+        });
+    }, 400);
+})(init);
