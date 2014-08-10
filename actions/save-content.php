a1289a6f4dcd3377e7fe684297b4358228ff618a
David Neilsen <david@panmedia.co.nz>
2014-08-11 08:57:07 +1200

    Remove old save files

--- actions/save-content.php
+++
@@ -1,5 +0,0 @@
-<?php
-include __DIR__ . '/../include.php';
-$example = new Raptor\Revision\Example('Save Content');
-$example->saveRevision(RAPTOR_DATA_DIR . '/content.json', RAPTOR_DATA_DIR . '/revisions.json');
-echo $example->saveJson(RAPTOR_DATA_DIR . '/content.json');
